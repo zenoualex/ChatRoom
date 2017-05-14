b4ea4afa0176524b5514d5e133cea680062826ea
ZeNAleX <alexanderpapa7@gmail.com>
2017-05-14 18:00:38 +0300

    Show Names on Chat

--- app/src/main/java/groupass/amc/chatroom/AddRoom.java
+++ app/src/main/java/groupass/amc/chatroom/AddRoom.java
@@ -29,13 +29,12 @@ import java.util.Set;
 
 public class AddRoom extends AppCompatActivity {
 
-    private Button  add_room;
+    private Button add_room;
     private EditText room_name;
 
     private ListView listView;
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> list_of_rooms = new ArrayList<>();
-    private String name;
     private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
 
     @Override
@@ -45,20 +44,20 @@ public class AddRoom extends AppCompatActivity {
 
         add_room = (Button) findViewById(R.id.btn_add_room);
         room_name = (EditText) findViewById(R.id.room_name_edittext);
-        listView  = (ListView) findViewById(R.id.listView);
+        listView = (ListView) findViewById(R.id.listView);
 
-        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list_of_rooms);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list_of_rooms);
 
         listView.setAdapter(arrayAdapter);
 
-        request_user_name();
+
 
         add_room.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Map<String,Object> map = new HashMap<String, Object>();
-                map.put(room_name.getText().toString(),"");
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put(room_name.getText().toString(), "");
                 root.updateChildren(map);
 
             }
@@ -71,8 +70,8 @@ public class AddRoom extends AppCompatActivity {
                 Set<String> set = new HashSet<String>();
                 Iterator i = dataSnapshot.getChildren().iterator();
 
-                while (i.hasNext()){
-                    set.add(((DataSnapshot)i.next()).getKey());
+                while (i.hasNext()) {
+                    set.add(((DataSnapshot) i.next()).getKey());
                 }
 
                 list_of_rooms.clear();
@@ -91,39 +90,11 @@ public class AddRoom extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                Intent intent = new Intent(getApplicationContext(),ChatRoom.class);
-                intent.putExtra("room_name",((TextView)view).getText().toString() );
-                intent.putExtra("user_name",name);
+                Intent intent = new Intent(getApplicationContext(), ChatRoom.class);
+                intent.putExtra("room_name", ((TextView) view).getText().toString());
                 startActivity(intent);
             }
         });
 
     }
-
-    private void request_user_name() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Enter name:");
-
-        final EditText input_field = new EditText(this);
-
-        builder.setView(input_field);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                name = input_field.getText().toString();
-            }
-        });
-
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-                request_user_name();
-            }
-        });
-
-        builder.show();
-    }
-
-
 }
